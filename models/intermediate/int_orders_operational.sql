77dc032dd735231236ac1f25af2d0627b1b7c3b5
Ylanit <myninita.shop@gmail.com>
2025-04-12 16:48:43 +0000

    operational margin error

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -6,12 +6,12 @@ select
     ,sm.quantity
     ,sm.purchase_price
     ,sm.purchase_cost
-    ,round(sm.margin + sh.shipping_fee - sh.logcost - CAST(sh.ship_cost AS FLOAT64),2) AS operational_margin
+    ,round(sm.margin + sh.shipping_fee - (sh.logcost + sh.ship_cost),2) AS operational_margin
     ,sh.shipping_fee
     ,sh.logcost
     ,CAST (sh.ship_cost AS FLOAT64) AS ship_cost
 from {{ ref("int_sales_margin") }} as sm
-join{{ ref("stg_raw__ship") }} as sh
+JOIN {{ ref("stg_raw__ship") }} as sh
 ON sm.orders_id = sh.orders_id
 
 
