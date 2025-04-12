2f37f66d2e6ff3b437554a144672495be1a1cc5c
Ylanit <myninita.shop@gmail.com>
2025-04-12 10:41:22 +0000

    ajout de margin dans sales_margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,3 +1,6 @@
-select *, quantity * purchase_price as purchase_cost
+select 
+*
+, quantity * purchase_price as purchase_cost
+, ROUND(revenue - (quantity * purchase_price),2) AS margin
 from {{ ref("stg_raw__sales") }}
 join{{ ref("stg_raw__product") }} using (products_id)
