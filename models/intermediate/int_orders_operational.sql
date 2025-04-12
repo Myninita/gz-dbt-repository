bb752265a3355b996b606d8603d13629efc30e58
Ylanit <myninita.shop@gmail.com>
2025-04-12 11:02:39 +0000

    calcul de operational margin ok

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -6,14 +6,15 @@ select
     ,sm.quantity
     ,sm.purchase_price
     ,sm.purchase_cost
-    ,sm.revenue - sm.purchase_cost  
+    ,round (sm.revenue - sm.purchase_cost  
       + sh.shipping_fee 
       - sh.logcost 
-      - CAST(sh.ship_cost AS FLOAT64) AS operational_margin
+      - CAST(sh.ship_cost AS FLOAT64),2) AS operational_margin
     ,sh.shipping_fee
     ,sh.logcost
     ,CAST (sh.ship_cost AS FLOAT64) AS ship_cost
 from {{ ref("int_sales_margin") }} as sm
 join{{ ref("stg_raw__ship") }} as sh
 ON sm.orders_id = sh.orders_id
+
 
