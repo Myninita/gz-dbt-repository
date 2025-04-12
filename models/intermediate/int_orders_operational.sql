5051a4715ae18d1a285b81cd6bf54a9f5058e03d
Ylanit <myninita.shop@gmail.com>
2025-04-12 10:56:57 +0000

    calcul in order sales operational

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,10 +1,20 @@
 select 
-    sm.*
+    sm.date_date
+    ,sm.products_id
+    ,sm.orders_id
+    ,sm.revenue
+    ,sm.quantity
+    ,sm.purchase_price
+    ,sm.purchase_cost
+    ,sm.margin
+    ,sm.margin 
+      + sh.shipping_fee 
+      - sh.logcost 
+      - CAST(sh.ship_cost AS FLOAT64) AS operational_margin
     ,sh.shipping_fee
     ,sh.logcost
     ,CAST (sh.ship_cost AS FLOAT64) AS ship_cost
-    , sh.shipping_fee  sh.logcost
 from {{ ref("int_sales_margin") }} as sm
 join{{ ref("stg_raw__ship") }} as sh
-ON sm.orders_id = sh.orders_id 
+ON sm.orders_id = sh.orders_id
 
