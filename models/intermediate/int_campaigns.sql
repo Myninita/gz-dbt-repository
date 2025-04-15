242e89b1cf98fd63a50d6b0183e89658207fdb2e
ylanit MALET <myninita.shop@gmail.com>
2025-04-15 16:10:02 +0000

    int_campaign _sql   union all campaign facebook  bing criteo and adwords

---
+++ models/intermediate/int_campaigns.sql
@@ -0,0 +1,16 @@
+SELECT
+*
+FROM  {{ref("stg_raw__adwords")}}
+UNION ALL
+SELECT 
+*
+FROM {{ref("stg_raw__criteo")}}
+UNION ALL
+SELECT
+*
+FROM {{ref("stg_raw__bing")}}
+UNION ALL
+SELECT
+*
+FROM {{ref("stg_raw__facebook")}}
+
