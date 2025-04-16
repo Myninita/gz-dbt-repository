7b18c51b1cb89bdb7f00e74960fe0fedbc2f500d
ylanit MALET <myninita.shop@gmail.com>
2025-04-16 17:53:40 +0000

    correction of margin ans util on schema

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,6 +1,7 @@
 select 
 *
 , quantity * purchase_price as purchase_cost
-, ROUND((revenue - purchase_price),2) AS margin
+, ROUND((revenue - (quantity * purchase_price)),2) AS margin
 from {{ ref("stg_raw__sales") }}
-join{{ ref("stg_raw__product") }} using (products_id)
+join {{ ref("stg_raw__product") }} using (products_id)
+ORDER BY margin ASC
