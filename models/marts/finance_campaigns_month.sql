d5b885752341a140c48feae0e54edc62a289c888
ylanit MALET <myninita.shop@gmail.com>
2025-04-16 13:58:03 +0000

    campagn day aggregated by day

--- models/marts/finance_campaigns_month.sql
+++ models/marts/finance_campaigns_month.sql
@@ -1,19 +1,16 @@
 SELECT
-datemonth
+date_date AS datemonth
 ads_margin
-average_basket
-operational_margin
-icd.ads_cost AS ads_cost
-icd.impression AS ads_impression
-icd.clicks AS ads_click
-quantity
-revenue
-purchase_cost
-margin
-fd.Total_shipping_fees AS shipping_fee
-fd.Total_log_costlogcost AS log_cost
-ship_cost
-FROM{{ref("int_campaigns_day")}} AS icd
-JOIN{{ref("finance_days")}} AS fd
+Average_Basket
+Operational_Margin
+ads_cost
+impression AS ads_impression
+clicks AS ads_click
+Total_nb_of_transactions AS quantity
+Total_revenue AS revenue
+Total_purchase_cost  AS purchase_cost
+Total_shipping_fees AS shipping_fee
+Total_log_costlogcost AS log_cost
+FROM{{ref("")}
 GROUP BY
 datemonth
